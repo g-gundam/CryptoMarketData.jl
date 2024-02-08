14673a6ceae2b8584172c71eb643d4ab6c41feeb
g-gundam <gg@nowhere>
2024-02-08 10:51:40 -0800

    Add helper functions for getting dates from filenames

--- src/helpers.jl
+++ src/helpers.jl
@@ -15,3 +15,20 @@ function last_csv(outdir)
     end
 end
 
+function _filename_to_date(f)
+    ds = replace(basename(f), ".csv" => "")
+    m = match(r"(\d{4})-(\d{2})-(\d{2})", ds)
+    Date(parse.(Int32, m.captures)...)
+end
+
+# date to index in span
+function _d2i(d::Date, cfs)
+    a = _filename_to_date(first(cfs))
+    b = _filename_to_date(last(cfs))
+    if a <= d <= b
+        diff = d - a
+        return diff.value + 1
+    else
+        missing
+    end
+end
