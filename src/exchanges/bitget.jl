6cb877a81bd687d20cc2c3198d146ebc784dc0ac
g-gundam <gg@nowhere>
2024-02-08 03:58:30 -0800

    Add the hacky bitget driver
    - using the undocumented front-end API to get 1m data
    - first to use proxy
    - weird adjustments to work around API weirdness

---
+++ src/exchanges/bitget.jl
@@ -0,0 +1,106 @@
+struct Bitget <: AbstractExchange
+    base_url::String
+    home_url::String
+    http_options::Dict
+
+    function Bitget()
+        new("https://api.bitget.com", "https://www.bitget.com", Dict())
+    end
+
+    function Bitget(http_options::Dict)
+        new("https://api.bitget.com", "https://www.bitget.com", http_options)
+    end
+end
+
+struct BitgetCandle <: AbstractCandle
+    ts::UInt64
+    o::Float64
+    h::Float64
+    l::Float64
+    c::Float64
+    v::Float64
+    v2::Float64
+end
+
+function ts2datetime_fn(bitget::Bitget)
+    DateTime ∘ unixmillis2nanodate
+end
+
+function candle_datetime(c::BitgetCandle)
+    unixmillis2nanodate(c.ts)
+end
+
+function short_name(bitget::Bitget)
+    "bitget"
+end
+
+function candles_max(bitget::Bitget)
+    1000
+end
+
+function get_markets(bitget::Bitget; type="dmcbl")
+    # type can be
+    # umcbl (usdt settled contracts)
+    # dmcbl (coin settled contracts)
+    # sumcbl (testnet usdt settled contracts)
+    # sdmcbl (testnet coin settled contracts)
+    info_url = bitget.base_url * "/api/mix/v1/market/contracts"
+    q = OrderedDict(
+        "productType" => type
+    )
+    uri = URI(info_url; query=q)
+    res = HTTP.get(uri)
+    json = JSON3.read(res.body)
+    return map(m -> m[:symbol], json[:data])
+end
+
+function get_candles(bitget::Bitget, market; start, stop, tf="1m", limit::Integer=10, tz_offset=(8 * 60 * 60 * 1000))
+    symbol = market
+    interval = if tf == "1d"
+        "1D"
+    elseif tf == "1m"
+        "1m"
+    else
+        "1m"
+    end
+    adjustment = if interval == "1m"
+        Minute(1)
+    else
+        Minute(0)
+    end
+    q = OrderedDict(
+        "kLineStep" => interval,
+        "kLineType" => 1,
+        "languageType" => 0,
+        "startTime" => nanodate2unixmillis(NanoDate(start)),
+        "endTime" => nanodate2unixmillis(NanoDate(stop) + adjustment),
+        "limit" => limit,
+        "symbolId" => symbol
+    )
+    # @info "get_candles" start q["startTime"] stop q["endTime"] limit
+    ohlc_url = bitget.home_url * "/v1/kline/getMoreKlineData"
+    uri = URI(ohlc_url)
+    headers = ["Content-Type" => "application/json"]
+    body = JSON3.write(q)
+    res = HTTP.post(uri, headers, body; bitget.http_options...)
+    json = JSON3.read(res.body)
+    effective_offset = if interval == "1D" # complete bullshit to work around bitget's timezone guessing
+        tz_offset
+    else
+        0
+    end
+    map(json.data) do c
+        BitgetCandle(
+            pui64(c[1]) + effective_offset,
+            pf64(c[2]),
+            pf64(c[3]),
+            pf64(c[4]),
+            pf64(c[5]),
+            pf64(c[6]),
+            pf64(c[7])
+        )
+    end
+end
+
+export Bitget
+export BitgetCandle
