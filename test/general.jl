83b812bbf8822735bda96139969b9c77b0e3ba30
g-gundam <g-gundam@users.noreply.github.com>
2025-09-24 06:27:17 -0700

    Add test for load tf summarization

--- test/general.jl
+++ test/general.jl
@@ -8,5 +8,16 @@ using Test
     @test df.market[1] == "BTCUSD"
     @test df.start[1] == Date("2011-08-18")
     @test df.market[2] == "ETHUSD"
-    @test ismissing(df.start[2])
+    @test df.start[2] == Date("2024-11-11")
+end
+
+@testset "load tf summarization" begin
+    bitstamp = Bitstamp()
+    ethusd = load(bitstamp, "ETHUSD"; span=Date("2024-11-11"):Date("2024-11-12"), tf=Hour(6))
+    @test size(ethusd) == (8, 6)
+    @test ethusd.o[1] == 3187.4
+    @test ethusd.h[2] == 3213.4
+    @test ethusd.l[3] == 3143.7
+    @test ethusd.c[4] == 3375.8
+    @test isapprox(ethusd.v[5], 3388.77; atol=0.1)
 end
