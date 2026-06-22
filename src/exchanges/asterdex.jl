1e78ab50d13fff6a470ab620646a5960638c05e1
g-gundam <g-gundam@users.noreply.github.com>
2026-06-22 06:17:26 -0700

    Implement the releatively easy part of Asterdex support
    This includes most of the parts needed for REST support
    except get_candles.

--- src/exchanges/asterdex.jl
+++ src/exchanges/asterdex.jl
@@ -18,7 +18,49 @@ end
     cts::UInt64                 # Close time
     qv::Float64                 # Quote asset volume
     trades::Int64               # Number of trades
-    tbvv::Flaot64               # Taker buy base asset volume
+    tbvv::Float64               # Taker buy base asset volume
     tbqv::Float64               # Taker buy quote asset volume
     ignore::Float64             # Ignore
 end
+
+function candle_type(asterdex::AsterdexFutures)
+    AsterdexFuturesCandle
+end
+
+function csv_headers(asterdex::AsterdexFutures)
+    collect(fieldnames(AsterdexFuturesCandle))
+end
+
+function csv_select(asterdex::AsterdexFutures)
+    1:6
+end
+
+function ts2datetime_fn(asterdex::AsterdexFutures)
+    DateTime ∘ unixmillis2nanodate
+end
+
+function candle_datetime(c::AsterdexFuturesCandle)
+    unixmillis2nanodate(c.ts)
+end
+
+function short_name(asterdex::AsterdexFutures)
+    "asterdex-futures"
+end
+
+function candles_max(asterdex::AsterdexFutures; tf=Minute(1))
+    1500
+end
+
+function get_markets(asterdex::AsterdexFutures)
+    url = asterdex.base_url * "/fapi/v3/exchangeInfo"
+    uri = URI(url)
+    res = HTTP.get(uri; asterdex.http_options...)
+    json = JSON3.read(res.body)
+    return map(m -> m[:pair], json[:symbols])
+end
+
+function get_candles(asterdex::AsterdexFutures, market; start, stop, tf=Minute(1), limit::Integer=10)
+end
+
+export AsterdexFutures
+export AsterdexFuturesCandle
