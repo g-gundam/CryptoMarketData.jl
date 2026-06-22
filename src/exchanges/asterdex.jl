9d8b0ff7bcd642f78702d1700274403dbe571433
g-gundam <g-gundam@users.noreply.github.com>
2026-06-21 21:13:53 -0700

    Add some constants

--- src/exchanges/asterdex.jl
+++ src/exchanges/asterdex.jl
@@ -1,4 +1,7 @@
-@kwdef struct Asterdex <: AbstractExchange
+ASTERDEX_FUTURES_V3_API = "https://fapi.asterdex.com"
+ASTERDEX_FUTURES_V3_WS_API = "wss://fstream.asterdex.com"
+
+@kwdef struct AsterdexFutures <: AbstractExchange
     base_url::AbstractString
     ws_url::AbstractString
     http_options::AbstractDict
